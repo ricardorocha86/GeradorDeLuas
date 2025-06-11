5760e3fc113ac8c50119c54f6483d83ed87253d8
Ricardo Rocha <ricardo8610@gmail.com>
2025-06-11 08:37:40 -0300

    Update aula.py

--- aula.py
+++ aula.py
@@ -1,18 +1,18 @@
 import streamlit as st
 
-st.title('Gerador de Luas')
+st.title('Calculadora de Calorias em Repouso')
 
 from sklearn import datasets
 
-n = st.slider('Tamanho amostral', 50, 1000, 500, 50)
+idade = st.slider('Idade', 16, 85, 20, 1)
+altura =  st.slider('Altura', 100, 250, 170, 1)
+peso =  st.slider('Peso', 10, 200, 70, 1)
 
-bagunca = st.slider('Bagunça nos dados', 0, 1000, 200, 50)
+#    homem: (13,75 x peso em quilos) + (5 x altura em centímetros) – (6,76 x idade em anos) + 66,5;
+#    mulher: (9,56 x peso em quilos) + (1,85 x altura em centímetros) – (4,68 x idade em anos) + 665.
 
-X, Y = datasets.make_moons(n_samples = n, noise = bagunca/1000, random_state = 1)
+caloria_homem = 13.75*peso + 5*altura - 6.76*idade + 66.5 
+caloria_mulher = 9.56*peso + 1.85*altura - 4.68*idade + 665 
 
-import matplotlib.pyplot as plt
-
-plt.scatter(X[:, 0], X[:, 1], c = Y, marker = 's', alpha = 0.5, cmap = 'Spectral')
-plt.axis('off') 
-
-st.pyplot()
+st.metric(label = 'Caloria p/ Homem', value = caloria_homem)
+st.metric(label = 'Caloria p/ Mulher', value = caloria_mulher)
